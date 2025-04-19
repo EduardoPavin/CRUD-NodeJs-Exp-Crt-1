840e70b730ca4d4effb365849e2f4a5c732ee04a
EduardoPavin <eduardopavin2010@hotmail.com>
2025-04-19 16:16:47 -0300

    feat(backend): Car models and logic created

--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
-// models/index.js
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('ExpCriativaCRUD', 'root', '12345678', {
   host: 'localhost',
-  port: 3306,   // se necessário
+  port: 3306,
   dialect: 'mysql',
 });
 
